63441aaa42e85f0624e346b5505ddd3ecc40357f
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-03 11:20:51 +0530

    basic-rendering-done

--- React Foundation/react-basic-project/filter-search/src/App.jsx
+++ React Foundation/react-basic-project/filter-search/src/App.jsx
@@ -1,34 +1,83 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+function Search() {
+  return (
+    <input type='text' placeholder='Search...' />
+  );
+}
+
+function Check() {
+  return (
+    <div>
+      <input type='checkbox' /> Only show product in stock
+    </div>
+  );
+}
+
+function SearchTable() {
+  return (
+    <div>
+      <Search />
+      <Check />
+    </div>
+  );
+}
+
+function ProductTable({items}) {
+  const fruits = [];
+  const vegetables = [];
+  items.map((products)=>{
+    if(products.category === "Fruits") {
+      fruits.push(products);
+    } else {
+      vegetables.push(products);
+    }
+  });
+  const fruitsEle = fruits.map((ele, id)=>{
+    let col = ele.stocked === true ? 'black' : 'red'; 
+    return (<li key={id}><span style={{color: col}}>{ele.name}</span> &nbsp;&nbsp;{ele.price}</li>)
+  });
+  const vegetableEle = vegetables.map((ele, id)=>{
+    let col = ele.stocked === true ? 'black' : 'red'; 
+    return (<li key={id}><span style={{color: col}} >{ele.name}</span> &nbsp;&nbsp;{ele.price}</li>)
+  });
+  return (
+    <div>
+      <p> <b>Name</b>  <b>Price</b></p>
+      <div>
+        <b>Fruits</b>
+        <ul className='ul'>
+          {fruitsEle}
+        </ul>
+      </div>
+      <div>
+        <b>Vegetables</b>
+        <ul className='ul'>
+          {vegetableEle}      
+        </ul>
+      </div>
+    </div>
+
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState(PRODUCTS);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <SearchTable />
+      <ProductTable items={items}/>
+    </div>
   )
 }
 
