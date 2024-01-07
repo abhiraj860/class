d62498357172792ac60df9b6aa6bc4507d1bbe84
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-07 15:54:13 +0530

    memoization

--- class-6.1/experiments/src/App.jsx
+++ class-6.1/experiments/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import React, {  useState } from 'react'
 
+const Header = React.memo(
+  
 function Header({title}) {
-  // console.log('rendered');
   return (
     <div>
       My name is &nbsp;{title}
     </div>
   );
 }
+);
 
-function HeaderWithButton() {
+
+
+function App() {
   const [name, setName] = useState('Abhiraj 1');
   function clickHandle() {
     setName(Math.random());
@@ -17,20 +21,11 @@ function HeaderWithButton() {
   return (
     <div>
       <button onClick={clickHandle}>Click me to change the title</button>
-      <Header title={name} /> 
-    </div>);
-}
-
-function App() {
-  
-  return (
-    <div>
-      <HeaderWithButton />  
-      <Header title={"Abhiraj 2"} />    
-      <Header title={"Abhiraj 3"} />    
-      <Header title={"Abhiraj 4"} />    
-      <Header title={"Abhiraj 5"} />    
-      <Header title={"Abhiraj 6"} />    
+      <Header title={name} />  
+      <Header title={"Abhiraj 2"} />  
+      <Header title={"Abhiraj 3"} />  
+      <Header title={"Abhiraj 4"} />  
+      <Header title={name} />  
     </div>
   );
 }
