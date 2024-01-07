f4ef5f618b4f4a7e5743f246fc3a57d0afbd0576
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-07 17:28:02 +0530

    card-wrapper

--- class-6.1/experiments/src/App.jsx
+++ class-6.1/experiments/src/App.jsx
@@ -1,49 +1,23 @@
-import React, {  useState } from 'react'
-
-
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 1,
-    title: 'go to gym',
-    description: 'go to gym'
-  }, {
-    id: 2,
-    title: 'tech',
-    description: 'techie'
-  }, {
-    id: 3,
-    title: 'hello',
-    description: 'say hello'
-  } 
-]);
-  
-function addTodo() {
-  setTodos([...todos, {
-    id: 4,
-    title: "abc",
-    description: "avfaasfd"
-  }]);
-}
-
-return (
-  <div>
-    <button onClick={addTodo}>Add a todo</button>
-    {todos.map(todo=> <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+  return <div>
+    <CardWrapper innerComponent={<TextComponenet/>} />
   </div>
-);
+}
+// console.log('here');
+
+function TextComponenet() {
+  return <div>
+    hi there
+  </div>
 }
 
-function Todo({title, description}) {
-  return (
-    <div>
-      <h1>
-        {title}
-      </h1>
-      <h5>
-        {description}
-      </h5>
-    </div>
-  );
+function CardWrapper({innerComponent}) {
+  return <div style={{
+    border: "2px solid black",
+    borderRadius: '10px'
+  }}>
+    {innerComponent}
+  </div>
 }
 
 export default App
