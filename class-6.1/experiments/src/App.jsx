28dfa43f2bd4de2c715456c4b134f83f25a2725c
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-07 15:44:20 +0530

    push-down-for-management

--- class-6.1/experiments/src/App.jsx
+++ class-6.1/experiments/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 function Header({title}) {
+  // console.log('rendered');
   return (
     <div>
       My name is &nbsp;{title}
@@ -8,19 +9,29 @@ function Header({title}) {
   );
 }
 
-
-
-function App() {
+function HeaderWithButton() {
   const [name, setName] = useState('Abhiraj 1');
   function clickHandle() {
     setName(Math.random());
   }
   return (
-    <>
+    <div>
       <button onClick={clickHandle}>Click me to change the title</button>
-      <Header title={name} />  
-      <Header title={"Abhiraj 2"} />  
-    </>
+      <Header title={name} /> 
+    </div>);
+}
+
+function App() {
+  
+  return (
+    <div>
+      <HeaderWithButton />  
+      <Header title={"Abhiraj 2"} />    
+      <Header title={"Abhiraj 3"} />    
+      <Header title={"Abhiraj 4"} />    
+      <Header title={"Abhiraj 5"} />    
+      <Header title={"Abhiraj 6"} />    
+    </div>
   );
 }
 
