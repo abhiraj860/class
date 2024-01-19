0b8cb374b27586b7572bfccf9ed00d2b1ef38886
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-19 23:54:21 +0530

    minor-change

--- 7.4-master/selector-family/src/atoms.js
+++ 7.4-master/selector-family/src/atoms.js
@@ -6,7 +6,7 @@ export const todosAtomFamily = atomFamily({
   default: selectorFamily({
     key: "todoSelectorFamily",
     get: (id) => async ({get}) => {
-      await new Promise((r)=>setTimeout(r, 5000));
+      await new Promise((r)=>setTimeout(r, 4000));
       // throw new Error('Backend call failed');
       const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
       return res.data.todo;
