5de518d9a5201eef8a61e41e947039d0d4d0f89e
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-21 11:00:26 +0530

    tailwind-class

---
+++ week8.1/tailwind-prac/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div style={{display: 'flex', justifyContent:'space-between'}}>
+      <div style={{backgroundColor: 'red'}}> hello</div>
+      <div style={{backgroundColor: 'green'}}> hello</div>
+      <div style={{backgroundColor: 'blue'}}> hello</div>
+      <div style={{backgroundColor: 'blue'}}> hello</div>
+      <div style={{backgroundColor: 'blue'}}> hello</div>
+    </div>
+  )
+}
+
+export default App
