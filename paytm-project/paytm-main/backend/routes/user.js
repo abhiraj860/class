a374c6ef83f0fa9e06fa9e940ef30ec684b7cd61
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-23 01:20:46 +0530

    sign-in-route-completed

--- paytm-project/paytm-main/backend/routes/user.js
+++ paytm-project/paytm-main/backend/routes/user.js
@@ -8,7 +8,7 @@ const z = require('zod');
 const router = Router();
 
 
-const userSchema = z.object({
+const signupSchema = z.object({
     username: z.string().email().min(5),
     firstName: z.string().min(1),
     lastName: z.string().min(1),
@@ -16,7 +16,7 @@ const userSchema = z.object({
 });
 
 router.post('/signup', async (req, res)=>{
-    const check = userSchema.safeParse(req.body);
+    const check = signupSchema.safeParse(req.body);
     const username = req.body.username;
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
@@ -25,13 +25,13 @@ router.post('/signup', async (req, res)=>{
     if(check.success) {
         const find = await User.findOne({username});
         if(!find) {
-            const token = jwt.sign(username, JWT_SECRET);
-            await User.create({
+            const user = await User.create({
                 username,
                 firstName,
                 lastName,
                 password
             });
+            const token = jwt.sign({userId: user._id}, JWT_SECRET);
             return res.status(200).json({
                 message: 'User created succesfully',
                 token
@@ -40,6 +40,29 @@ router.post('/signup', async (req, res)=>{
     }
     return res.status(411).json({
         message: 'Email already taken / Incorrect inputs'
+    });
+});
+
+const signInSchema = z.object({
+    username: z.string().email().min(2),
+    password: z.string().min(1)
+});
+
+router.post('/signin', async(req, res)=>{
+    const verify = signInSchema.safeParse(req.body);
+    if(verify.success) {
+        const username = req.body.username;
+        const password = req.body.password;
+        const find = await User.findOne({username, password});
+        if(find) {
+            const token = jwt.sign({userId: find._id}, JWT_SECRET);
+            return res.status(200).json({
+                token
+            });
+        }
+    }
+    return res.status(411).json({
+        message: 'Error while logging in'
     });
 });
 
