a25ed3fffd7ba554a0c886dc0d6f6d649fcb032f
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-24 23:17:50 +0530

    added-get-balance

--- paytm-project/paytm-main/backend/routes/account.js
+++ paytm-project/paytm-main/backend/routes/account.js
@@ -1,7 +1,16 @@
 const {Router} = require('express');
+const {authMiddleware} = require('./middleware');
+const {Account} = require('../db');
+
 const router = Router();
 
-
+router.get('/balance', authMiddleware,async (req, res)=>{
+    const {userId} = req;
+    const balance = await Account.findOne({userId},{ _id: 0, userId: 0, __v:0 }).select('balance');
+    res.status(200).json({
+        balance : balance.balance
+    });
+});
 
 
 
