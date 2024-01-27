293ad5f8284a57e69e2e1deaef53ba3edd633c29
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-27 22:16:59 +0530

    sign-in-page-completed

--- paytm-project/paytm-main/frontend/src/pages/SignIn.jsx
+++ paytm-project/paytm-main/frontend/src/pages/SignIn.jsx
@@ -1,7 +1,21 @@
+import {Heading} from '../components/Heading'
+import {SubHeading} from '../components/SubHeading'
+import {InputBox} from '../components/InputBox'
+import {Button} from '../components/Button'
+import {BottomWarning} from '../components/BottomWarning'
+
 export function SignIn() {
     return (
-        <div>
-            From SignIn
+        <div className='h-screen bg-gray-400 flex justify-center'>
+            <div className='h-3/5 p-2 bg-white w-96 rounded-lg mt-9'>
+                <Heading text={"Sign In"}/>
+                <SubHeading text={"Enter your credentials to access your account"} />
+                <br></br>
+                <InputBox name={"Email"} placeholder={"abhiraj@gmail.com"}/>
+                <InputBox name={"Password"} placeholder={"123456"}/>
+                <Button text={"Sign In"}/>
+                <BottomWarning text={"Don't have an account?"} link={"Sign Up"} to={'/signup'} />
+            </div>
         </div>
     )
 }
