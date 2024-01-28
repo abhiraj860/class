d5b88b040f6339fc562207c4ec470c0ddef6307a
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-28 12:02:00 +0530

    bits-of-backend

--- paytm-project/paytm-main/frontend/src/pages/Send.jsx
+++ paytm-project/paytm-main/frontend/src/pages/Send.jsx
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom"
+
 export function Send() {
+    const [searchParams] = useSearchParams();
+    const id = searchParam.get("id");
+    const name = searchParams.get("name");
     return (
         <div className="flex justify-center">
             <div className="w-96 h-80 mt-16 shadow-2xl shadow-gray-400 rounded-lg">
@@ -9,11 +14,11 @@ export function Send() {
                     <div className="flex justify-start items-center">
                         <div className='w-10 h-10  flex items-center justify-center rounded-full text-white bg-green-500 text-lg'>
                             <div>
-                                A
+                                {name[0].toUpperCase()}
                             </div>
                         </div>
                         <div className="font-bold text-xl ml-3">
-                            Friend's Name
+                            {name}
                         </div>
                     </div>
                     <div className="font-medium text-sm mt-1">
@@ -23,7 +28,16 @@ export function Send() {
                         <input type="number" className="border-2 rounded-lg w-5/6 p-2" placeholder="Enter amount" />
                     </div>
                     <div className="mt-3">
-                        <button type="button" className="w-5/6 focus:outline-none text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-green-800">Initiate Transfer</button>
+                        <button onClick={async ()=>{
+                            axios.post('http://localhost:3000/api/v1/account/transfer', {
+                                to: id,
+                                amount
+                            }, {
+                                headers: {
+                                    Authorization: "Bearer " + localStorage.getItem("token")
+                                }
+                            });
+                        }} type="button" className="w-5/6 focus:outline-none text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-green-800">Initiate Transfer</button>
 
                     </div>
             
