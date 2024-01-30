1caea1a047079528802e1586b087133e1aa0801e
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-01-31 02:02:04 +0530

    ts-done

--- class-9.1/typescript/node-app/src/a.ts
+++ class-9.1/typescript/node-app/src/a.ts
@@ -1,15 +1,4 @@
-function getEle<T>(args: T[]):T {
-    return args[0];
-}
+import a, {b} from './b'
 
-interface User{
-    name: string
-}
-
-const first = getEle<User>([{name:"Abhiraj"}, {name:"Aditya"}]);
-const first2 = getEle<number>([1, 2]);
-const first3 = getEle<boolean>([true, false]);
-
-console.log(first);
-console.log(first2);
-console.log(first3);
+console.log(a);
+console.log(b);
