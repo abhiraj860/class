9638909ff23133c6e8667072326b8de0cf37d925
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-06 17:13:52 +0530

    joins-in-sql-queries

--- SQL Queries/queries.sql
+++ SQL Queries/queries.sql
@@ -4,3 +4,13 @@ FROM mytable
 WHERE condition(s)
 ORDER BY column ASC/DESC
 LIMIT num_limit OFFSET num_offset;
+
+-- JOINs
+
+SELECT column, another_table_column, …
+FROM mytable
+INNER JOIN another_table 
+    ON mytable.id = another_table.id
+WHERE condition(s)
+ORDER BY column, … ASC/DESC
+LIMIT num_limit OFFSET num_offset;
