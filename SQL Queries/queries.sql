7971a3b6fc6588c95ddd8cd195ba000b97414eab
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-06 20:50:29 +0530

    sql-queries-done

--- SQL Queries/queries.sql
+++ SQL Queries/queries.sql
@@ -32,3 +32,101 @@ FROM mytable
 WHERE column IS/IS NOT NULL
 AND/OR another_condition
 AND/OR …;
+
+-- Working with expression
+
+SELECT col_expression AS expr_description, …
+FROM mytable;
+
+SELECT column AS better_column_name, …
+FROM a_long_widgets_table_name AS mywidgets
+INNER JOIN widget_sales
+ON mywidgets.id = widget_sales.widget_id;
+
+-- Aggregate function
+
+SELECT AGG_FUNC(column_or_expression) AS aggregate_description, …
+FROM mytable
+WHERE constraint_expression;
+
+-- GROUP BY
+
+SELECT AGG_FUNC(column_or_expression) AS aggregate_description, …
+FROM mytable
+WHERE constraint_expression
+GROUP BY column;
+
+-- Having clause
+
+SELECT group_by_column, AGG_FUNC(column_expression) AS aggregate_result_alias, …
+FROM mytable
+WHERE condition
+GROUP BY column
+HAVING group_condition;
+
+-- Complete select query
+
+SELECT DISTINCT column, AGG_FUNC(column_or_expression), …
+FROM mytable
+JOIN another_table
+    ON mytable.column = another_table.column
+WHERE constraint_expression
+GROUP BY column
+HAVING constraint_expression
+ORDER BY column ASC/DESC
+LIMIT count OFFSET COUNT;
+
+-- INSERT operation
+
+INSERT INTO mytable
+VALUES (value_or_expr, another_value_or_expr, …),
+       (value_or_expr_2, another_value_or_expr_2, …),
+       …;
+
+INSERT INTO mytable
+(column, another_column, …)
+VALUES (value_or_expr, another_value_or_expr, …),
+      (value_or_expr_2, another_value_or_expr_2, …),
+      …;
+
+-- Updating rows
+UPDATE mytable
+SET column = value_or_expr, 
+    other_column = another_value_or_expr, 
+    …
+WHERE condition;
+
+-- DELETING rows
+DELETE FROM mytable
+WHERE condition;
+
+-- CREATING Tables
+CREATE TABLE IF NOT EXISTS mytable (
+    column DataType TableConstraint DEFAULT default_value,
+    another_column DataType TableConstraint DEFAULT default_value,
+    …
+);
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    director TEXT,
+    year INTEGER, 
+    length_minutes INTEGER
+);
+
+-- ALTERING table
+
+ALTER TABLE mytable
+ADD column DataType OptionalTableConstraint 
+    DEFAULT default_value;
+
+ALTER TABLE mytable
+DROP column_to_be_deleted;
+
+ALTER TABLE mytable
+RENAME TO new_table_name;
+
+-- DROPPING Tables
+
+DROP TABLE IF EXISTS mytable;
