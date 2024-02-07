04802606289f36f8c51481f3b9f6fcd27357c530
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-08 02:33:21 +0530

    arrays-in-ts

--- tsc-practice/dist/index.js
+++ tsc-practice/dist/index.js
@@ -1,6 +1,23 @@
 "use strict";
-function printId(id) {
-    console.log(`ID: ${id}`);
+;
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 19
+    },
+    {
+        firstName: "Jane",
+        lastName: "Smith",
+        age: 30
+    },
+    {
+        firstName: "Bob",
+        lastName: "Johnson",
+        age: 22
+    }
+];
+function getLegal(user) {
+    return user.filter(value => (value.age > 18 && value));
 }
-console.log('12131313');
-console.log(123123123424323424);
+console.log(getLegal(users));
--- tsc-practice/src/index.ts
+++ tsc-practice/src/index.ts
@@ -1,17 +1,29 @@
-type Employee = {
-    name: string;
-    startDate: Date;
-}
-
-type Manager = {
-    name: string;
-    department: string;
-}
-
-type TeamLead = Employee & Manager;
-
-const team: TeamLead = {
-    name: "Abhiraj",
-    startDate: new Date(),
-    department: "Software dev"
+interface User {
+    firstName: string;
+    lastName: string;
+    age: number;
 };
+
+const users: User[] = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 19
+    },
+    {
+        firstName: "Jane",
+        lastName: "Smith",
+        age: 30
+    },
+    {
+        firstName: "Bob",
+        lastName: "Johnson",
+        age: 22
+    }
+];
+
+function getLegal(user: User[]) : User[] {
+    return user.filter(value=>(value.age > 18 && value));
+}
+
+console.log(getLegal(users));
