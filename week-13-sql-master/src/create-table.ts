c1c18418499034ef5368db5fa9547f5cb25b6cbe
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-09 18:05:04 +0530

    create-table

--- week-13-sql-master/src/create-table.ts
+++ week-13-sql-master/src/create-table.ts
@@ -7,8 +7,10 @@ async function createTable() {
     const createUserTableQuery = `
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
             email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
         );
     `;
 
@@ -32,5 +34,5 @@ async function createTable() {
 }
 
 
+// createTable();
 
-createTable();
