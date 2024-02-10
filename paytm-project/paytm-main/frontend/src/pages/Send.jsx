1694a619645ba427412639bc1913b416f551ee15
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-11 00:37:07 +0530

    send-navigation-done

--- paytm-project/paytm-main/frontend/src/pages/Send.jsx
+++ paytm-project/paytm-main/frontend/src/pages/Send.jsx
@@ -1,12 +1,14 @@
 import { useSearchParams } from "react-router-dom"
 import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Send() {
     const [searchParams] = useSearchParams();
     const id = searchParams.get("id");
     const name = searchParams.get("name");
     const [amount, setamount] = useState(0);
+    const navigate = useNavigate();
     return (
         <div className="flex justify-center">
             <div className="w-96 h-80 mt-16 shadow-2xl shadow-gray-400 rounded-lg">
@@ -32,14 +34,20 @@ export function Send() {
                     </div>
                     <div className="mt-3">
                         <button onClick={async ()=>{
-                            await axios.post('http://localhost:3000/api/v1/account/transfer', {
-                                to: id,
-                                amount
-                            }, {
-                                headers: {
-                                    Authorization: "Bearer " + localStorage.getItem("token")
-                                }
-                            });
+                            try {
+                                await axios.post('http://localhost:3000/api/v1/account/transfer', {
+                                    to: id,
+                                    amount
+                                }, {
+                                    headers: {
+                                        Authorization: "Bearer " + localStorage.getItem("token")
+                                    }
+                                }).then(resp=>alert(resp.data.message));
+                                navigate('/dashboard');
+                            } catch(error) {
+                                alert(error.response.data.message);
+                            }
+                            
                             
                         }} type="button" className="w-5/6 focus:outline-none text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-500 dark:hover:bg-green-700 dark:focus:ring-green-800">Initiate Transfer</button>
 
