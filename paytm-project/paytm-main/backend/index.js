e9e4c60945a1f919c462af0716d4c4d129286c3d
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-15 10:17:51 +0530

    simple-format

--- paytm-project/paytm-main/backend/index.js
+++ paytm-project/paytm-main/backend/index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const {PORT} = require('./routes/config.js'); 
+const express = require("express");
+const { PORT } = require("./routes/config.js");
 
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 
-
-const mainRouter = require('./routes/index.js');
+const mainRouter = require("./routes/index.js");
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1', mainRouter);
+app.use("/api/v1", mainRouter);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
