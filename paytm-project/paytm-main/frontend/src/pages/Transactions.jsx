b278b13443088c7e123833a66cc1d339fe0c9272
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-17 21:16:05 +0530

    list-done

--- paytm-project/paytm-main/frontend/src/pages/Transactions.jsx
+++ paytm-project/paytm-main/frontend/src/pages/Transactions.jsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
 import { Backbutton } from "../components/Backbutton";
+import { Loader } from "../components/Loader";
 import axios from "axios";
 
 export function Transactions() {
-	const [transac, setTransac] = useState([{}]);
-	
-
-	useEffect(()=>{
-		axios.get('http://localhost:3000/api/v1/account/getTransaction', {
-			headers: {
-				authorization: "Bearer " + localStorage.getItem("token")
-			}
-		
-		}).then((res)=>{setTransac((t)=>res.data.statement.transactions)}).catch((error)=>console.log("No data found"));
+	const [list, setList] = useState([{}]);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		axios
+			.get("http://localhost:3000/api/v1/account/getTransaction", {
+				headers: {
+					authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			})
+			.then((res) => {
+				setList((t) => res.data.statement.transactions);
+				setLoading(() => false);
+			})
+			.catch((error) => console.log("No data found"));
 	}, []);
 
+	return (
+		<div>{loading === true ? <Loader /> : <TransactionPage list={list} />}</div>
+	);
+}
+
+function TransactionPage({ list }) {
 	return (
 		<div>
 			<div className="flex justify-center">
@@ -32,72 +43,38 @@ export function Transactions() {
 					<div className="text-black">Statement</div>
 				</div>
 			</div>
-			<div className="flex justify-center">
-				<div className="border-2 rounded-xl h-[450px] w-3/5 overflow-auto">
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
+			<div>
+				<div className="flex justify-center">
+					<div className="border-2 rounded-xl h-[450px] w-3/5 overflow-auto">
+						{list.map((value) => (
+							<List
+								firstName={value.firstName}
+								lastName={value.lastName}
+								amount={value.amount}
+								received={value.received}
+							/>
+						))}
 					</div>
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function List({ firstName, lastName, amount, received }) {
+	return (
+		<div>
+			{console.log(firstName, lastName, amount, received)}
+			<div className=" font-bold text-xl ">
+				<div className="flex justify-between items-center border-b-2 p-5">
+					<div className="text-black">
+						{firstName} {lastName}
 					</div>
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
-					</div>
-					<div>
-						<div className=" font-bold text-xl ">
-							<div className="flex justify-between items-center border-b-2 p-5">
-								<div className=" text-black">Abhiraj</div>
-								<div className=" text-red-500">-1000</div>
-							</div>
-						</div>
-					</div>
+					{received === true ? (
+						<div className="text-green-500">{"+"}{amount}</div>
+					) : (
+						<div className="text-red-500">{amount}</div>
+					)}
 				</div>
 			</div>
 		</div>
