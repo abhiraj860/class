b2202cf2685b5e4bcbc670cc8582b647434730df
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-02-19 22:25:55 +0530

    tsc-react

--- react-tsc/react-tsc/src/App.tsx
+++ react-tsc/react-tsc/src/App.tsx
@@ -1,7 +1,28 @@
-export function App() {
+ export const App: React.FC = () => {
 	return (
 		<div>
+			<TextField text="hello" person={{firstName:"abhira", lastName:"sdfsd"}} />	
+		</div>
+	)
+}
 
+interface Person {
+	firstName: string;
+	lastName: string;
+}
+
+interface Props {
+	text: string;
+	ok?: boolean;
+	i?: number;
+	fn?: (bob: string)=> string;
+	person: Person;
+}
+
+const TextField:React.FC< Props > = () =>{
+	return (
+		<div>
+			<input />
 		</div>
 	)
 }
