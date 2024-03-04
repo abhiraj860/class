8e4f09aec63e20b86c95a68aae01b726e180ae35
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-04 23:33:48 +0530

    auth-banner-aded

--- nextjs-frontend/next-app/app/layout.tsx
+++ nextjs-frontend/next-app/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="p-4 bottom-b">
+          Medium website
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
