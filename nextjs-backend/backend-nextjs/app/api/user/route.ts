7ba7e38e88cb9b6e2ff4e340217bce2d3a8504c5
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-05 18:55:23 +0530

    router-add

---
+++ nextjs-backend/backend-nextjs/app/api/user/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server";
+
+
+export async function POST(req:NextRequest) {
+    
+    // extract the body
+    const body = await req.json();
+    
+    // store the body in the database
+    console.log(body);
+
+    return Response.json({
+        message: "You are logged in"
+    });
+}
