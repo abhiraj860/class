9fd25ce431a8f4f23e5589700ff940cfc7a1b1e9
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-05 23:36:04 +0530

    database

--- nextjs-backend/backend-nextjs/app/page.tsx
+++ nextjs-backend/backend-nextjs/app/page.tsx
@@ -1,23 +1,9 @@
-import axios from "axios";
-
-async function getUserDetails() {
-  // await new Promise(r=>setTimeout(r, 5000));
-  const response = await axios.get("http://localhost:3000/api/user")
-	return response.data;
-}
-
-// async component
-export default async function Home() {
-  const userData = await getUserDetails();
+export default  function Home() {
   return (
      <div className="flex flex-col justify-center h-screen">
         <div className="flex justify-center">
             <div className="border p-8 rounded">
-                <div>
-                    Name: {userData?.name}
-                </div>
-                
-                {userData?.email}
+              Hello this is the root route
             </div>
         </div>
     </div>
