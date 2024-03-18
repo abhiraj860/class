f851120d05d847cbdeba0181611f02643c0a47ce
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-03-18 23:45:07 +0530

    react

--- React/Class React/src/App.jsx
+++ React/Class React/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-      <div>
-        <button onClick={() => setCount(Math.random())}>
-          count is {count}
-        </button>
-      </div>
-  )
+	return (
+		<div>
+			<button onClick={() => setCount(Math.random())}>count is {count}</button>
+		</div>
+	);
 }
 
-export default App
+export default App;
