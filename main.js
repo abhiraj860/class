d6fe2a1a088d5b57a75f15fb84c61417161c22aa
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-02 23:47:08 +0530

    function

--- main.js
+++
@@ -1,5 +0,0 @@
-const arr = [1, 2, 3, 4, 5];
-const p = [1, 2, 3, 4, 5];
-
-console.log(arr == p);
-console.log(arr === p);
---
+++ main.ts
@@ -0,0 +1,9 @@
+const p = () => {
+    return 5;
+}
+
+const p1 = ()=> {
+    return 5;
+}
+
+console.log(p === p1);
