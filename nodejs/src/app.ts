c3947f8a12690d9b543ae7d00a60331bb6c4ce24
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-05 23:58:51 +0530

    fs

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,9 +1,1 @@
-import http from 'http';
-import url from 'url';
-
-http.createServer((req, res)=>{
-    res.writeHead(220, {'content-abhi' : 'abhi'});
-    const q = url.parse(req.url || "", true).query;
-    const txt = q.year + " " + q.month;
-    res.end(txt);
-}).listen(8080);
+import fs from 'fs';
