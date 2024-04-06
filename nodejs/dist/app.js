1db95d70d6779e1f282676b879ab5eabccfcaa87
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-06 10:56:25 +0530

    added

--- nodejs/dist/app.js
+++ nodejs/dist/app.js
@@ -3,11 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
 const http_1 = __importDefault(require("http"));
-const url_1 = __importDefault(require("url"));
 http_1.default.createServer((req, res) => {
-    res.writeHead(220, { 'content-abhi': 'abhi' });
-    const q = url_1.default.parse(req.url || "", true).query;
-    const txt = q.year + " " + q.month;
-    res.end(txt);
+    fs_1.default.readFile('../nodejs/demo1.html', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        res.writeHead(200, { "Abhiraj": "abdid" });
+        res.write(data);
+        return res.end();
+    });
 }).listen(8080);
--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,1 +1,13 @@
 import fs from 'fs';
+import http from 'http';
+
+http.createServer((req, res)=>{
+    fs.readFile('../nodejs/demo1.html', (err, data)=>{
+        if(err) {
+            console.log(err);
+        }
+        res.writeHead(200, {"Abhiraj": "abdid"});
+        res.write(data);
+        return res.end();        
+    });
+}).listen(8080);
