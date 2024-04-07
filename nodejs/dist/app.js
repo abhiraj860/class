183527ef6d33fdaf39505fcda32520db9593465b
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-07 11:23:21 +0530

    serve-file

--- nodejs/dist/app.js
+++ nodejs/dist/app.js
@@ -3,9 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const url_1 = __importDefault(require("url"));
 const fs_1 = __importDefault(require("fs"));
-fs_1.default.rename('myrenamedFile.txt', 'abhiraj.txt', (err) => {
-    if (err)
-        throw err;
-    console.log('File renamed!!');
-});
+const http_1 = __importDefault(require("http"));
+http_1.default.createServer((req, res) => {
+    const qdata = url_1.default.parse(req.url || "", true);
+    const path = qdata.pathname;
+    const createPath = '.' + path + '.html';
+    fs_1.default.readFile(createPath, (err, file) => {
+        if (err) {
+            res.writeHead(404, { 'Content-type': 'text/html' });
+            res.write('404 page');
+            return res.end();
+        }
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        return res.end(file);
+    });
+}).listen(3000);
--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -1,6 +1,18 @@
+import url from 'url';
 import fs from 'fs';
+import http from 'http';
 
-fs.rename('myrenamedFile.txt', 'abhiraj.txt', (err)=>{
-    if(err) throw err;
-    console.log('File renamed!!');
-});
+http.createServer((req, res)=>{
+    const qdata = url.parse(req.url || "" , true);
+    const path = qdata.pathname;
+    const createPath = '.' + path + '.html'; 
+    fs.readFile(createPath, (err, file)=>{
+        if(err) {
+            res.writeHead(404, {'Content-type': 'text/html'});
+            res.write('404 page');
+            return res.end();
+        }
+        res.writeHead(200, {'Content-type' : 'text/html'});
+        return res.end(file);
+    })
+}).listen(3000);
