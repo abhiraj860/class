d38ef4fc9c8d9a801c753b13a5ba700da01ce5c5
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-19 02:25:24 +0530

    swagger-ui

--- openAPI-spec/index.ts
+++ openAPI-spec/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { openApiSpec } from './openapispec';
 
 const app = express();
 const port = 3000;
@@ -6,20 +8,21 @@ const port = 3000;
 app.use(express.json());
 
 let users = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Doe' }
+	{ id: 1, name: 'John Doe' },
+	{ id: 2, name: 'Jane Doe' }
 ];
 
 app.get('/users', (req, res) => {
-    const { name }: {name?: string} = req.query;
-    if (name) {
-        const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
-        res.json(filteredUsers);
-    } else {
-        res.json(users);
-    }
+	const { name }: {name?: string} = req.query;
+	if (name) {
+		const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
+		res.json(filteredUsers);
+	} else {
+		res.json(users);
+	}
 });
 
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(openApiSpec))
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+	console.log(`Server running on http://localhost:${port}`);
 });
