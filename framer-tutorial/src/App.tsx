45f6ca381ea2d64b7fe6b5996e57ecac54813d6a
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-24 01:33:39 +0530

    framer-motion

---
+++ framer-tutorial/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div className='w-40 h-40 bg-red-400'>
+      </div>
+    </div>
+  )
+}
+
+export default App
