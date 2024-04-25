8930e80487cf92fb28a8d65999c839da98ed157f
Abhiraj Aditya <abhiaditya860@gmail.com>
2024-04-25 21:47:04 +0530

    basic-changes

--- framer-tutorial/src/App.tsx
+++ framer-tutorial/src/App.tsx
@@ -1,11 +1,10 @@
 import { motion } from "framer-motion";
-
 function App() {
 	return (
 		<div className="flex justify-center">
 			<motion.div
         animate={{rotate:[0, 200, 200, 0], x:[0, 200, 200, 0, -200, -200, 0]}}
-        transition={{repeat: Infinity, duration: 3}}
+        transition={{repeat: Infinity, duration: 2}}
         className="rounded-2xl w-40 h-40 bg-red-400 mt-20"
 			></motion.div>
 		</div>
